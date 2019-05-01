18f157e06d728d27fd325dac21099fd81b103b16
BlazingHoundoom <guernsey624@gmail.com>
2019-05-01 17:31:16 -0600

    Updates

---
+++ precache-manifest.a1877645f9f4db8d14d280128caa8683.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fcf63f88b2235306ce165bf464ec6cf",
+    "url": "/lupusebrius/static/media/LupusEbriusLogo.4fcf63f8.svg"
+  },
+  {
+    "revision": "64bd4b6fca370e242530",
+    "url": "/lupusebrius/static/js/runtime~main.fdc999dd.js"
+  },
+  {
+    "revision": "e6979222dec865520bc2",
+    "url": "/lupusebrius/static/js/main.a1e6e022.chunk.js"
+  },
+  {
+    "revision": "cce8ebd36996366c892e",
+    "url": "/lupusebrius/static/js/2.7ee2c62e.chunk.js"
+  },
+  {
+    "revision": "e6979222dec865520bc2",
+    "url": "/lupusebrius/static/css/main.7748a950.chunk.css"
+  },
+  {
+    "revision": "c523ffc35891139e606130bab36faa77",
+    "url": "/lupusebrius/index.html"
+  }
+];
